a460d2b5d7f9b855e4b8e154141a9cc013d6fbf9
sosow0212 <sosow0212@naver.com>
2021-10-09 22:47:27 +0900

    HTTP 응답 데이터 - API JSON

---
+++ src/main/java/hello/servlet/basic/reponse/ResponseJsonServlet.java
@@ -0,0 +1,32 @@
+package hello.servlet.basic.reponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //Content-Type: application/json
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+
+        HelloData hellodata = new HelloData();
+        hellodata.setUsername("Lee");
+        hellodata.setAge(20);
+
+        // {"username":"Lee", "age":20}
+        String result = objectMapper.writeValueAsString(hellodata);
+        response.getWriter().write(result);
+    }
+}
